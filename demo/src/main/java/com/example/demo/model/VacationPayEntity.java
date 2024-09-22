49f5d001510e7a5c12ca536679bc36e6f9c99c02
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2024-09-22 17:01:59 +0300

    init commit

--- demo/src/main/java/com/example/demo/model/VacationPayEntity.java
+++ demo/src/main/java/com/example/demo/model/VacationPayEntity.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
